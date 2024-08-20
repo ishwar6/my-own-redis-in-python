466a73a0a15f30ce85d89a7f1a934549cc5d4980
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2024-08-20 20:49:51 +0530

    conn removed

--- networking/server.py
+++ networking/server.py
@@ -6,11 +6,6 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379),   reuse_port=True)
     conn, addr = server_socket.accept()
-
-    # with conn:
-    #     conn.recv(1024)
-    #     conn.send(pong.encode())
-
     while True:
         request: bytes = conn.recv(512)
         data:str = request.decode()
